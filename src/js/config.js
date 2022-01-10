9fbbcba9679fd1600f715ae1c06c40f1ae10b97d
Pablo <tackpablo@gmail.com>
2022-01-10 13:23:17 -0800

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9e5f747f-f2b6-4e5c-8a38-ab5545bd25e8';
+export const MODAL_CLOSE_SEC = 2.5;
